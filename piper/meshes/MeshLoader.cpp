42ded0429325a9b0cd5c2156165308ba4abba249
Joshua Augustin <augustinspring@gmail.com>
2018-02-17 16:58:13 -0500

    Mesh loading.

--- piper/meshes/MeshLoader.cpp
+++ piper/meshes/MeshLoader.cpp
@@ -1,6 +1,91 @@
 #include "meshes/MeshLoader.hpp"
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+// adapted from https://stackoverflow.com/a/37454181
+std::vector<std::string> split(const std::string& str, const std::string& delim, bool allow_empty = false) {
+  std::vector<std::string> tokens;
+  size_t prev = 0, pos = 0;
+  do {
+    pos = str.find(delim, prev);
+    if (pos == std::string::npos) pos = str.length();
+    std::string token = str.substr(prev, pos-prev);
+    if (!token.empty() || allow_empty) tokens.push_back(token);
+    prev = pos + delim.length();
+  } while (pos < str.length() && prev < str.length());
+  return tokens;
+}
+
+
+Eigen::Vector2f vector2f_from_line(const std::vector<std::string>& splits) {
+  return {
+    std::stof(splits.at(1)),
+    std::stof(splits.at(2)),
+  };
+}
+
+
+Eigen::Vector3f vector3f_from_line(const std::vector<std::string>& splits) {
+  return {
+    std::stof(splits.at(1)),
+    std::stof(splits.at(2)),
+    std::stof(splits.at(3)),
+  };
+}
+
 
 std::unique_ptr<Mesh> MeshLoader::load(const path& path) {
-  return nullptr;
+  aligned_vector<Eigen::Vector3f> positions;
+  aligned_vector<Eigen::Vector2f> texture_coordinates;
+  aligned_vector<Eigen::Vector3f> normals;
+  aligned_vector<Eigen::Vector3i> indices;
+
+  std::ifstream file(path);
+
+  if (file.bad()) {
+    std::cerr << "Failed to open file " << path << std::endl;
+    return nullptr;
+  }
+
+  try {
+    std::string line;
+    for(size_t i = 0; std::getline(file, line); ++i) {
+      std::vector<std::string> parts = split(line, " ");
+      std::istringstream oss(line);
+      std::string type;
+      oss >> type;
+
+      if (type == "v") {
+        positions.emplace_back(vector3f_from_line(parts));
+        printf("Line %02d: v %f %f %f\n", i, positions.back().x(), positions.back().y(), positions.back().z());
+      } else if (type == "vn") {
+        normals.emplace_back(vector3f_from_line(parts));
+        printf("Line %02d: vn %f %f %f\n", i, normals.back().x(), normals.back().y(), normals.back().z());
+      } else if (type == "vt") {
+        texture_coordinates.emplace_back(vector2f_from_line(parts));
+        printf("Line %02d: vt %f %f\n", i, texture_coordinates.back().x(), texture_coordinates.back().y());
+      } else if (type == "f") {
+        Eigen::Vector3i face;
+        for (size_t i = 1; i < 4; ++i) {
+          std::vector<std::string> vertex_parts = split(parts.at(i), "/", true);
+          int index = std::stoi(vertex_parts.front()); // only vertex matters;
+          face(i - 1) = index;
+        }
+        indices.emplace_back(face);
+        printf("Line %02d: f %d %d %d\n", i, face.x(), face.y(), face.z());
+      } else {
+        printf("Line %02d: Skipped '%s'\n", i, line.c_str());
+      }
+    }
+  } catch (...) {
+    return nullptr;
+  }
+
+  return std::make_unique<Mesh>(std::move(positions),
+                                std::move(texture_coordinates),
+                                std::move(normals),
+                                std::move(indices));
 }
